8f4614ee18ad2cf53317fb1aca1e153143e464b8
Robb <robbsala@hotmail.com>
2018-01-05 23:07:05 -0800

    Fri Changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,10 @@
 var latitude;
 var longitude;
+var map;
+var radius = 3 //in kilometers
 
 function initAutocomplete() {
-  var map = new google.maps.Map(document.getElementById('map'), {
+  map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 37.621313, lng: -122.378955},
     zoom: 13,
     mapTypeId: 'roadmap'
@@ -39,10 +41,6 @@ function initAutocomplete() {
     places.forEach(function(place) {
       if (!place.geometry) {
         console.log("Returned place contains no geometry");
-        latitude = place.geometry.viewport.b.b;
-        console.log(latitude);
-        longitude = place.geometry.viewport.b.f;
-        console.log(longitude);
         return;
       }
       var icon = {
@@ -53,10 +51,8 @@ function initAutocomplete() {
         scaledSize: new google.maps.Size(25, 25)
       };
 
-        latitude = place.geometry.viewport.b.b;
-        console.log(latitude);
-        longitude = place.geometry.viewport.b.f;
-        console.log(longitude);
+      longitude = place.geometry.viewport.b.b;
+      latitude = place.geometry.viewport.f.b;
 
       // Create a marker for each place.
       markers.push(new google.maps.Marker({
@@ -76,6 +72,72 @@ function initAutocomplete() {
     map.fitBounds(bounds);
   });
 }
+
+
+$(document).on('click','#search_button', function(){
+  var type = $('#search_button').data("type");
+  console.log(type)
+  console.log(latitude)
+  console.log(longitude)
+
+  var queryURL = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location='+latitude+','+longitude+'&radius='+radius*1000+'&type='+type+'&key=AIzaSyDZFVJF-MiHZ5CyrDPgTYj3ibc5MoTgMZg'
+  console.log(queryURL);
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).done(function(responce) {
+    console.log(responce);
+    console.log(responce.status)
+    console.log("==================================");
+
+    // Loop through and set markers on map
+    for (var i = 0; i < responce.results.length; i++) {
+        var lat = responce.results[i].geometry.location.lat;
+        var lng = responce.results[i].geometry.location.lng;
+        console.log('-------------------------------------')
+
+        var marker = new google.maps.Marker({
+          position: {lat: lat, lng: lng},
+          map: map,
+          title: 'Hello World!'
+        })
+
+         $.ajax({
+         url: 'https://api.uber.com/v1.2/estimates/price',
+         headers: {
+           'Authorization': 'Bearer KA.eyJ2ZXJzaW9uIjoyLCJpZCI6InUrQ294MS9tUkcyZXEwZkFERXdrZnc9PSIsImV4cGlyZXNfYXQiOjE1MTYyNTc4NDQsInBpcGVsaW5lX2tleV9pZCI6Ik1RPT0iLCJwaXBlbGluZV9pZCI6MX0.beOa_9qarmZzh1ouo27s-5m7A9lk89EKQhDrBEcrLSk',
+           'Accept-Language': 'en_US',
+           'Access-Control-Allow-Origin': '*'
+         },
+         dataType: 'json',
+         data: {
+           start_latitude: latitude,
+           start_longitude: longitude,
+           end_latitude: lat,
+           end_longitude: lng,
+         }
+         }).done(function(data) {
+             console.log(data);
+             console.log(data.estimate);
+             var price_x= data.prices[7].estimate;
+             console.log('price for uber X: '+price_x);
+             var duration= data.prices[7].duration;
+             console.log('duration is '+duration / 60 +' min');
+        });
+
+    }
+  });
+})
+
+
+
+
+
+
+
+
+
+
 
 
 
